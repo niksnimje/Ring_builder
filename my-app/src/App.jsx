a405d25b38b851b4b7e64b29212e3b2b06e8e645
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2026-04-25 07:25:06 +0000

    update path

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import HomePage from "./components/Home/HomePage";
+// import HomePage from "./components/Home/HomePage";
 import DiamondPage from "./components/DiamondPage/DiamondPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/Home/HomePage";
+import RingBuilder from "./components/RingBulider/RingBuilder";
 
 const App = () => {
 
@@ -10,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/ring-builder" element={<RingBuilder />} />
           <Route path="/diamond" element={<DiamondPage />} />
         </Routes>
       </BrowserRouter>
