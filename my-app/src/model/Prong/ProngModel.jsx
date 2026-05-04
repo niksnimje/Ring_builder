80b17c6f9d2c5d2133779fc00680448d543b6bd4
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2026-05-04 11:16:49 +0000

    update path ssimilar

--- my-app/src/model/Prong/ProngModel.jsx
+++ my-app/src/model/Prong/ProngModel.jsx
@@ -4,9 +4,11 @@ import { Color, Box3, Vector3 } from "three";
 import { useFrame } from "@react-three/fiber";
 import AnimatedDiamond from "./AnimatedDiamond";
 import * as THREE from "three";
+import FadeWrapper from "../../utils/FadeWrapper";
 
 const ProngModel = ({
   modelPath,
+  fadeTrigger,
   color = "#fefbf2",
   scale = 0.24,
   position = [0, 0, 0],
@@ -36,32 +38,65 @@ const ProngModel = ({
 
     const tempDiamonds = [];
 
+    // cloned.traverse((child) => {
+    //   if (child.isMesh) {
+    //     const name = child.name.toLowerCase();
+
+    //     if (name.includes("diamond") || name.includes("gem") || name.includes("Created_with_OctoNus_software_(httpwwwoctonuscom)")) {
+    //       child.visible = false;
+    //       child.updateWorldMatrix(true, false);
+
+    //       const box = new Box3().setFromObject(child);
+    //       const size = new Vector3();
+    //       box.getSize(size);
+    //       const volume = size.x * size.y * size.z;
+
+    //       tempDiamonds.push({
+    //         geometry: child.geometry,
+    //         matrix: child.matrixWorld.clone(),
+    //         volume: volume,
+    //         name: child.name
+    //       });
+    //     } else {
+    //       child.material = child.material.clone();
+    //       child.material.color = new Color(color);
+    //       Object.assign(child.material, sharedMetalProps || {});
+    //     }
+    //   }
+    // });
+
     cloned.traverse((child) => {
-      if (child.isMesh) {
-        const name = child.name.toLowerCase();
+  if (child.isMesh) {
+    const name = child.name.toLowerCase();
+    
+    // console.log("Mesh Name:", name);
 
-        if (name.includes("diamond") || name.includes("gem")) {
-          child.visible = false;
-          child.updateWorldMatrix(true, false);
+    const isDiamond = name.includes("diamond") || 
+                      name.includes("gem") || 
+                      name.includes("octonus"); 
 
-          const box = new Box3().setFromObject(child);
-          const size = new Vector3();
-          box.getSize(size);
-          const volume = size.x * size.y * size.z;
+    if (isDiamond) {
+      child.visible = false;
+      child.updateWorldMatrix(true, false);
 
-          tempDiamonds.push({
-            geometry: child.geometry,
-            matrix: child.matrixWorld.clone(),
-            volume: volume,
-            name: child.name
-          });
-        } else {
-          child.material = child.material.clone();
-          child.material.color = new Color(color);
-          Object.assign(child.material, sharedMetalProps || {});
-        }
-      }
-    });
+      const box = new Box3().setFromObject(child);
+      const size = new Vector3();
+      box.getSize(size);
+      const volume = size.x * size.y * size.z;
+
+      tempDiamonds.push({
+        geometry: child.geometry,
+        matrix: child.matrixWorld.clone(),
+        volume: volume,
+        name: child.name
+      });
+    } else {
+      child.material = child.material.clone();
+      child.material.color = new Color(color);
+      Object.assign(child.material, sharedMetalProps || {});
+    }
+  }
+});
 
     tempDiamonds.sort((a, b) => b.volume - a.volume);
 
@@ -79,12 +114,13 @@ const ProngModel = ({
   }, [scene, color, sharedMetalProps, setProngDiamondName]);
 
   return (
+     <FadeWrapper trigger={fadeTrigger}>
     <group scale={scale} position={position}  
     onClick={(e) => {
     e.stopPropagation();
 
     if (onProngClick) {
-      // world position nikal
+      // world position 
       const worldPos = new THREE.Vector3();
       e.eventObject.getWorldPosition(worldPos);
 
@@ -105,6 +141,7 @@ const ProngModel = ({
         />
       ))}
     </group>
+    </FadeWrapper>
   );
 };
 
